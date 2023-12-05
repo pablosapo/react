e4d40f62e72dcf6f2c164625c7c383d0a9452b65
pablosapo <pablo@dror.cl>
2023-12-05 19:57:50 -0300

    cambio menu

--- src/componentes/menu/Menu.jsx
+++ src/componentes/menu/Menu.jsx
@@ -12,7 +12,7 @@ const Menu = () =>{
         <ul>
             <li> <Link to="/"> Home</Link> </li>
             <li> <Link to="/carrocompras"> Productos</Link> </li>
-            <li> <Link to="/personal"> Personal</Link> </li>
+            <li> <Link to="/personal"> Nosotros</Link> </li>
             <li> <Link to="/login"> Login </Link> </li>
             <li> <Link to="/registro"> Registro </Link> </li>
         </ul>
