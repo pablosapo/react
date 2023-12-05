f55919bdc0f87dcb830f4940a0e125348e221fb6
pablosapo <pablo@dror.cl>
2023-12-05 16:58:46 -0300

    cambio de menu

--- src/componentes/menu/Menu.jsx
+++ src/componentes/menu/Menu.jsx
@@ -1,9 +1,14 @@
-import { Link } from "react-router-dom";
 import './Menu.css';
+import Logo from './img/logo.png';
+import { Link } from 'react-router-dom';
 
 const Menu = () =>{
-    return(   
+    return(
+        
         <nav className="menu">
+        <div>
+                <img src={Logo} alt="Logo" />
+            </div>
         <ul>
             <li> <Link to="/"> Home</Link> </li>
             <li> <Link to="/carrocompras"> Productos</Link> </li>
